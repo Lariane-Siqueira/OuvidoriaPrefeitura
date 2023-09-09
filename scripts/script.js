f752c4e65251f5364c434d64b4e664d7e975b644
unknown <larianesiqueira9a@gmail.com>
2023-09-09 16:57:17 -0300

    formulario

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let btnEnviar = document.getElementById("btn-enviar").addEventListener("click", enviar);
+let inputs = document.getElementsByClassName("dados")[0]
+
+function enviar() {
+    if(inputs.hasAttribute("required")){
+        
+    }
+}
